e87b1e90eac817198a2f2f42035bbe84bd6717de
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-31 23:57:17 +0300

    img

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const localAPI = "http://localhost:3000/kmovies";
 const localAPIpopular = "http://localhost:3000/popular";
 let kDramaData = [];
+const nowairing = document.getElementsByClassName("wrapper");
+console.log(nowairing);
 const imageContainer = document.getElementsByClassName("carosel-img");
 
 console.log(imageContainer);
+const recentlyRel = document.getElementsByClassName("recentreleases");
+const getKdramaData = () => {
+  return fetch(localAPI).then((response) => response.json());
+};
 
-const getKdramaData = () => { return fetch(localAPI).then((response) => response.json());};
-
-
-function updateCurrently(currentlyAiring) { for (let i = 0; i <= currentlyAiring.length; i++) {imageContainer[i].src = currentlyAiring[i].poster;}}
-
+// function updateCurrently(currentlyAiring) { for (let i = 0; i <= currentlyAiring.length; i++) {imageContainer[i].src = currentlyAiring[i].poster;}}
 
 document.addEventListener("DOMContentLoaded", async () => {
   kDramaData = await getKdramaData();
-  console.log(kDramaData);
+  console.log("KdramaData:", kDramaData);
 
   let recentrelease = kDramaData.filter((movie) => {
     if (movie.releasedYear == "2019") {
@@ -27,25 +29,35 @@ document.addEventListener("DOMContentLoaded", async () => {
       return movie;
     }
   });
-  console.log(populareleases);
+  console.log("popularDATA", populareleases);
 
   let currentlyAiring = kDramaData.filter((movie) => {
     if (movie.releasedYear == "2022") {
       return movie;
     }
   });
-  
 
-  console.log(currentlyAiring);
+  console.log("CUURENTAIRING:", currentlyAiring);
 
-  updateCurrently(currentlyAiring);
+  const contain = document.querySelector(".container");
+
+  const updateUI = (currentlyAiring) => {
+    const data = currentlyAiring.map((_element, _index) => {
+      console.log(_element.title);
+      return `<div class="wrapper" key=${_index}>
+      <div class="movie-card">
+        <div class="movieimg">
+          <img src=" ${_element.poster}">
+        </div>
+        <div class="moviedetail">
+          <h5 class="movie-card-title">
+             ${_element.title}
+          </h5>
+        </div>
+      </div>
+    </div>`;
+    });
+    contain.innerHTML = data;
+  };
+  updateUI(currentlyAiring);
 });
-
-{/* <div class="movie-card">
-          <div class="movieimg">
-            <img>
-          </div>
-          <div class="moviedetail">
-            <h5 class="movie-card-title"></h5>
-          </div>
-</div> */}
