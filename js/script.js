1bad862dd46812e19926c43b7a92fb81269917b5
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-31 16:51:01 +0300

    Add slides

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 const localAPI = "http://localhost:3000/kmovies";
 const localAPIpopular = "http://localhost:3000/popular";
 let kDramaData = [];
-let popular = [];
+const imageContainer = document.getElementsByClassName("carosel-img");
 
-const getKdramaData = () => {
-  return fetch(localAPI).then((response) => response.json());
-};
-const getPopular = () => {
-  return fetch(localAPIpopular).then((response) => response.json());
-};
+console.log(imageContainer);
+
+const getKdramaData = () => { return fetch(localAPI).then((response) => response.json());};
+
+
+function updateCurrently(currentlyAiring) { for (let i = 0; i <= currentlyAiring.length; i++) {imageContainer[i].src = currentlyAiring[i].poster;}}
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   kDramaData = await getKdramaData();
   console.log(kDramaData);
 
-  popular = await getPopular();
-  console.log(popular);
+  let recentrelease = kDramaData.filter((movie) => {
+    if (movie.releasedYear == "2019") {
+      return movie;
+    }
+  });
+  console.log(recentrelease);
+
+  let populareleases = kDramaData.filter((movie) => {
+    if (movie.percentLiked >= 90) {
+      return movie;
+    }
+  });
+  console.log(populareleases);
+
+  let currentlyAiring = kDramaData.filter((movie) => {
+    if (movie.releasedYear == "2022") {
+      return movie;
+    }
+  });
+  
+
+  console.log(currentlyAiring);
+
+  updateCurrently(currentlyAiring);
 });
+
+{/* <div class="movie-card">
+          <div class="movieimg">
+            <img>
+          </div>
+          <div class="moviedetail">
+            <h5 class="movie-card-title"></h5>
+          </div>
+</div> */}
